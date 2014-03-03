680326df08745926b70696ef9e95b381268c0cf1
Patrick Robertson <robertson.patrick@gmail.com>
2014-03-03 12:58:57 +0000

    Trivial change to copyright year

--- Paldaruo/UTIAppDelegate.h
+++ Paldaruo/UTIAppDelegate.h
@@ -3,7 +3,7 @@
 //  Paldaruo
 //
 //  Created by Dewi Bryn Jones on 27.12.2013.
-//  Copyright (c) 2013 Uned Technolegau Iaith. All rights reserved.
+//  Copyright (c) 2014 Uned Technolegau Iaith. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
