00228de943f994a7a41086066b6a8b161f25a6a1
David Chan <david@troi.org>
2014-03-17 12:43:47 +0000

    Promtiau newydd a newid bach i'r API llwytho i fyny

--- python-server/HttpServer.py
+++ python-server/HttpServer.py
@@ -1,3 +1,5 @@
+#!python
+#encoding: utf-8
 import os, re, traceback, BaseHTTPServer, SocketServer, json, sys, cgi, uuid, sqlite3
 
 class AuthError(Exception):
@@ -5,7 +7,13 @@ class AuthError(Exception):
 
 class TorfRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path != '/':
+
+        htmlFile='test.html'
+        print self.path;
+
+        if self.path == '/telerau_v1.0.html':
+            htmlFile='telerau_v1.0.html'            
+        elif self.path != '/':
             self.send_response(404)
             self.end_headers()
             self.wfile.close()
@@ -13,7 +21,7 @@ class TorfRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
-        with open('test.html') as f:
+        with open(htmlFile) as f:
             self.wfile.writelines(f)
         self.wfile.close()
 
@@ -202,20 +210,45 @@ class TorfRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def getOutstandingPrompts(self, uid):
         return [
-                {"identifier": "sample1", "text": "AGOR AGOR CAU CAU AGOR AGOR CAU CAU"},
-                {"identifier": "sample2", "text": "CHWITH CHWITH DE DE CHWITH CHWITH DE DE"}, 
-                {"identifier": "sample3", "text": "I FYNY, I FYNY, I LAWR, I LAWR, I FYNY, I FYNY, I LAWR, I LAWR"},
-                {"identifier": "sample4", "text": "LAN, LAN, I LAWR, I LAWR, LAN, LAN, I LAWR, I LAWR"},
-                {"identifier": "sample5", "text": "GAFAEL GAFAEL ARDDWRN ARDDWRN GAFAEL GAFAEL ARDDWRN ARDDWRN"},
-                {"identifier": "sample6", "text": "YSGWYDD YSGWYDD PENELIN PENELIN YSGWYDD YSGWYDD PENELIN PENELIN"},
-                {"identifier": "sample7", "text": "GOLAU, YMLAEN, GOLAU, I FFWRDD, GOLAU, YMLAEN, GOLAU, I FFWRDD"},
-                {"identifier": "sample8", "text": "GOLAU YMLAEN GOLAU BANT GOLAU YMLAEN GOLAU BANT"}, 
-                {"identifier": "sample9", "text": "GAFAEL AGOR GAFAEL CAU GAFAEL AGOR GAFAEL CAU"},
-                {"identifier": "sample10", "text": "PENELIN, I FYNY, PENELIN, I LAWR, YSGWYDD, I FYNY, YSGWYDD, I LAWR"},
-                {"identifier": "sample11", "text": "PENELIN, LAN, PENELIN, I LAWR, YSGWYDD, LAN, YSGWYDD, I LAWR"},
-                {"identifier": "sample12", "text": "ARDDWRN DE ARDDWRN CHWITH ARDDWRN DE ARDDWRN CHWITH"},
+                {"identifier": "sample1", "text": u"AGOR, AGOR, CAU, CAU, AGOR, AGOR, CAU, CAU"},
+                {"identifier": "sample2", "text": u"I'R CHWITH, I'R DDE, I'R DDE, I'R CHWITH, I'R CHWITH, I'R DDE"}, 
+                {"identifier": "sample3", "text": u"I FYNY, I FYNY, I LAWR, I LAWR, I FYNY, I FYNY, I LAWR, I LAWR"},
+                {"identifier": "sample4", "text": u"LAN, LAN, I LAWR, I LAWR, LAN, LAN, I LAWR, I LAWR"},
+                {"identifier": "sample5", "text": u"YMLAEN, YMLAEN, YMLAEN, YMLAEN"},
+                {"identifier": "sample6", "text": u"GAFAEL, GAFAEL, ARDDWRN, ARDDWRN, GAFAEL, GAFAEL, ARDDWRN, ARDDWRN"},
+                {"identifier": "sample7", "text": u"YSGWYDD, YSGWYDD, PENELIN, PENELIN, YSGWYDD, YSGWYDD, PENELIN, PENELIN"},
+                {"identifier": "sample8", "text": u"GOLAU, GOLAU, TROI, TROI, GOLAU, GOLAU, TROI, TROI"},
+                {"identifier": "sample9", "text": u"GAFAEL AGOR, GAFAEL CAU, GAFAEL AGOR, GAFAEL CAU"},
+                {"identifier": "sample10", "text": u"PENELIN I FYNY, PENELIN I LAWR, YSGWYDD I FYNY, YSGWYDD I LAWR"},
+                {"identifier": "sample11", "text": u"PENELIN LAN, PENELIN I LAWR, YSGWYDD LAN, YSGWYDD I LAWR"},
+                {"identifier": "sample12", "text": u"ARDDWRN I FYNY, ARDDWRN I LAWR, ARDDWRN I FYNY, ARDDWRN I LAWR"},
+                {"identifier": "sample13", "text": u"ARDDWRN LAN, ARDDWRN I LAWR, ARDDWRN LAN, ARDDWRN I LAWR"},
+                {"identifier": "sample14", "text": u"TROI I'R CHWITH, TROI I'R DDE, TROI I'R CHWITH, TROI I'R DDE"},
+                {"identifier": "sample15", "text": u"AFAL, MAM, GORAU, GEMAU"},  
+                {"identifier": "sample16", "text": u"MÂN, GADAEL, BLAEN, HADAU"},
+                {"identifier": "sample17", "text": u"DAU, AWR, DYSGWYR, DIGWYDD"},       
+                {"identifier": "sample18", "text": u"CLOI, PENODOL , SEREN, I GYD"},     
+                {"identifier": "sample19", "text": u"MELIN, GORAU, BANGOR, ASGWRN"},     
+                {"identifier": "sample20", "text": u"HYNNY, CATH, DEG, ADDAS"},  
+                {"identifier": "sample21", "text": u"DDOE, GADAEL, AGOS, LARWM"},
+                {"identifier": "sample22", "text": u"ALAW, AFAL, YMLACIO, ANODD"},   
+                {"identifier": "sample23", "text": u"BRAN, PEDWERYDD, AWR, BANGOR"}, 
+                {"identifier": "sample24", "text": u"BARN, TRO, YMLACIO, FYCHAN"},
+                {"identifier": "sample25", "text": u"AFAL, MYNYDD, TRO"},
+                {"identifier": "sample26", "text": u"YSGOL, ASGWRN, CHWAER, CATH"},
+                {"identifier": "sample27", "text": u"CHWECH, AFAL, MAM, GORAU"},
+                {"identifier": "sample28", "text": u"GEMAU, MÂN, GADAEL, BLAEN"},
+		        {"identifier": "sample29", "text": u"HADAU, DAU, AWR, DYSGWYR"},
+                {"identifier": "sample30", "text": u"DIGWYDD, CLOI, PENODOL, SEREN"}, 
+                {"identifier": "sample31", "text": u"I GYD, MELIN, GORAU, BANGOR"},
+                {"identifier": "sample32", "text": u"ASGWRN, HYNNY, CATH, DEG"},
+                {"identifier": "sample33", "text": u"ADDAS, DDOE, GADAEL, AGOS"},
+                {"identifier": "sample34", "text": u"LARWM, ALAW, AFAL, YMLACIO"}, 
+                {"identifier": "sample35", "text": u"ANODD, BRAN, PEDWERYDD, AWR"}, 
+                {"identifier": "sample36", "text": u"BANGOR, BARN, TRO, YMLACIO"},
+                {"identifier": "sample37", "text": u"FYCHAN, MYNYDD, TRO, YSGOL"},
+                {"identifier": "sample38", "text": u"ASGWRN, CHWAER, CATH, AFAL"},
         ]
-
 
     def savePrompt(self, uid, promptId, fileData):
         if not re.match(r"^[0-9A-Za-z_]+$", promptId):
@@ -228,6 +261,11 @@ class TorfRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         promptFile = os.path.join(userDir, promptId + '.wav')
         with open(promptFile, 'w') as f:
             f.write(fileData)
+
+        print uid + ", " + promptId
+
+        filename=promptId+".wav" 
+        return {"fileId":filename}
 
 class HttpServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     def __init__(self, hostAndPort, dbFile, storeDir):
