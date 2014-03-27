0e98d96f0019df94cf7b5d51d79702583fe06b60
Patrick Robertson <robertson.patrick@gmail.com>
2014-03-27 11:40:50 +0000

    Peidio parhau os mae application error ar weinydd Paldaruo yn ystod llwytho i fyny. Hefyd, cynnwys fideo o ddefnyddio’r ap.

--- Paldaruo/UTIViewController.m
+++ Paldaruo/UTIViewController.m
@@ -86,6 +86,11 @@
                                                  name:@"InternetUnreachable"
                                                object:nil];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleInternetUnreachable:)
+                                                 name:@"PaldaruoServerApplicationError"
+                                               object:nil];
+    
     [UTIReachability instance];
     
 
@@ -387,6 +392,14 @@
     
 }
 
+-(void)handlePaldaruoServerApplicationError:(NSNotification *)notification {
+    [self.lblOutletNextPrompt setEnabled:NO];
+    [self.lblOutletRecordingStatus setEnabled:NO];
+    [self.btnOutletMoveToNextRecordingState setEnabled:NO];
+    [self.btnOutletRedoRecording setEnabled:NO];
+    [self.lblOutletProfileName setEnabled:NO];
+    [self.lblOutletSessionProgress setEnabled:NO];
+}
 
 @end
 
