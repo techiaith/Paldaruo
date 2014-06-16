c29eaac81a4a6c562777b9f5ed1abae011ddaa12
Dewi Bryn Jones <dewibrynjones@Auri-MacBook-Pro.local>
2014-06-16 18:50:05 +0100

    gwella’r cod JSON y testunau

--- Paldaruo/UTIDataStoreOffline.m
+++ Paldaruo/UTIDataStoreOffline.m
@@ -13,21 +13,26 @@
 
 + (void)http_fetchOutstandingPrompts:(UTIPromptsTracker *)prompts useridentifier:(NSString *)ident {
     
-    NSArray *hardcodePrompts = [NSArray arrayWithObjects:
-                                @"hen gwlad tadau annwyl mi",
-                                @"tatws moron pys cig",
-                                @"gwyliau haul traeth tywod",
-                                nil];
     
-    for (int x=0; x < hardcodePrompts.count; x++)
+    NSDictionary *json = @{@"response":@[
+                                       @{@"identifier":@"sample1", @"text":@"HEN GWLAD TADAU ANNWYL MI"},
+                                       @{@"identifier":@"sample2", @"text":@"TATWS BLODAU MORON MEFUS"}
+                                       ]
+                           };
+    
+    NSArray *jsonArray = json[@"response"];
+    
+    // initialise the prompts tracker.
+    for (int x = 0; x < jsonArray.count; x++)
     {
-        UTIPrompt *newPrompt = [[UTIPrompt alloc] init];
-        newPrompt.text = [hardcodePrompts objectAtIndex:x];
-        newPrompt.identifier = [NSString stringWithFormat:@"offline-sample-%d",x];
+        UTIPrompt* newPrompt=[[UTIPrompt alloc] init];
         
+        newPrompt.text = [[jsonArray objectAtIndex:x] objectForKey:@"text"];
+        newPrompt.identifier = [[jsonArray objectAtIndex:x] objectForKey:@"identifier"];
+        
+        //[self.prompts addPromptForRecording:newPrompt];
         [prompts addPromptForRecording:newPrompt];
     }
-    
 
 }
 
@@ -39,6 +44,7 @@
 
 + (void)http_getMetadata:(NSString *)uid sender:(id <UTIErrorReporter>)sender {
     // do nothing
+    
 }
 
 
