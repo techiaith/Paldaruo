2875b3b6ac9730d6b4dd24c0bf3c10bec067448f
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2017-08-03 14:49:05 +0100

    ychwanegu promtiau / adding more prompts

--- python-server/paldaruo-serve-forever.sh
+++ python-server/paldaruo-serve-forever.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true; do
     START=`date +%s`
-    /usr/local/bin/python webserver.py
+    python webserver.py
     END=`date +%s`
     ELAPSED=`expr $END - $START`
     if [ "$ELAPSED" -lt 10 ] ; then
